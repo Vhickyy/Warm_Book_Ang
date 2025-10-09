e6b97da6a3040500724625ebf100c42e5ce52c25
Victoria Okonnah <victoriaokonnah@gmail.com>
2025-10-09 18:22:15 +0100

    ci fail 2

--- karma.conf.js
+++ karma.conf.js
@@ -1,48 +1,8 @@
-module.exports = function (config) {
-  config.set({
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    browsers: ["ChromeHeadless"],
-    reporters: ["progress", "kjhtml", "coverage"], // Added "coverage" here
-    coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage"),
-      subdir: ".",
-      reporters: [
-        { type: "html" },
-        { type: "text-summary" },
-        { type: "lcovonly" },
-      ],
-      check: {
-        global: {
-          statements: 100,
-          branches: 100,
-          functions: 100,
-          lines: 100,
-        },
-      },
-    },
-  });
-};
-
-// // Karma configuration file
 // module.exports = function (config) {
 //   config.set({
-//     basePath: "",
 //     frameworks: ["jasmine", "@angular-devkit/build-angular"],
-//     plugins: [
-//       require("karma-jasmine"),
-//       require("karma-chrome-launcher"),
-//       require("karma-jasmine-html-reporter"),
-//       require("karma-coverage"),
-//     ],
-//     client: {
-//       jasmine: {
-//         // Jasmine configuration
-//       },
-//       clearContext: false,
-//     },
-//     jasmineHtmlReporter: {
-//       suppressAll: true,
-//     },
+//     browsers: ["ChromeHeadless"],
+//     reporters: ["progress", "kjhtml", "coverage"], // Added "coverage" here
 //     coverageReporter: {
 //       dir: require("path").join(__dirname, "./coverage"),
 //       subdir: ".",
@@ -51,7 +11,6 @@ module.exports = function (config) {
 //         { type: "text-summary" },
 //         { type: "lcovonly" },
 //       ],
-//       // Enforce 100% coverage - CI will fail if below these thresholds
 //       check: {
 //         global: {
 //           statements: 100,
@@ -61,13 +20,54 @@ module.exports = function (config) {
 //         },
 //       },
 //     },
-//     reporters: ["progress", "kjhtml", "coverage"],
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     browsers: ["Chrome"],
-//     singleRun: false,
-//     restartOnFileChange: true,
 //   });
 // };
+
+// Karma configuration file
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+    ],
+    client: {
+      jasmine: {
+        // Jasmine configuration
+      },
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "lcovonly" },
+      ],
+      // Enforce 100% coverage - CI will fail if below these thresholds
+      check: {
+        global: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100,
+        },
+      },
+    },
+    reporters: ["progress", "kjhtml", "coverage"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["Chrome"],
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
