ce00ca90de6b83df206ee699527abe66921d0f8f
Victoria Okonnah <victoriaokonnah@gmail.com>
2025-10-09 19:02:13 +0100

    ci fail

--- karma.conf.js
+++ karma.conf.js
@@ -22,59 +22,41 @@
 //     },
 //   });
 // };
-
-// module.exports = function (config) {
-//   config.set({
-//     basePath: "",
-//     frameworks: ["jasmine", "@angular-devkit/build-angular"],
-//     plugins: [
-//       require("karma-jasmine"),
-//       require("karma-chrome-launcher"),
-//       require("karma-jasmine-html-reporter"),
-//       require("karma-coverage-istanbul-reporter"),
-//     ],
-//     client: {
-//       clearContext: false,
-//     },
-//     coverageIstanbulReporter: {
-//       dir: require("path").join(__dirname, "./coverage"),
-//       reports: ["html", "lcovonly", "text-summary"],
-//       fixWebpackSourcePaths: true,
-//       thresholds: {
-//         emitWarning: false,
-//         global: {
-//           statements: 100,
-//           lines: 100,
-//           branches: 100,
-//           functions: 100,
-//         },
-//       },
-//     },
-//     reporters: ["progress", "coverage-istanbul"],
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     browsers: ["ChromeHeadless"],
-//     singleRun: false,
-//     restartOnFileChange: true,
-//   });
-// };
-
-// karma.conf.js
+// Karma configuration file
 module.exports = function (config) {
   config.set({
-    // ... other configurations
-    coverageReporter: {
-      // ... other coverage configurations
-      check: {
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+    ],
+    client: {
+      clearContext: false,
+    },
+    coverageIstanbulReporter: {
+      dir: require("path").join(__dirname, "./coverage"),
+      reports: ["html", "lcovonly", "text-summary"],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        emitWarning: false,
         global: {
           statements: 100,
+          lines: 100,
           branches: 100,
           functions: 100,
-          lines: 100,
         },
       },
     },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["ChromeHeadless"],
+    singleRun: false,
+    restartOnFileChange: true,
   });
 };
