fbe88bd8a5d074a0cad371cca2f418999e5b69c0
Victoria Okonnah <victoriaokonnah@gmail.com>
2025-10-09 18:49:35 +0100

    ci fail

--- karma.conf.js
+++ karma.conf.js
@@ -23,35 +23,50 @@
 //   });
 // };
 
-// Karma configuration file
+// module.exports = function (config) {
+//   config.set({
+//     basePath: "",
+//     frameworks: ["jasmine", "@angular-devkit/build-angular"],
+//     plugins: [
+//       require("karma-jasmine"),
+//       require("karma-chrome-launcher"),
+//       require("karma-jasmine-html-reporter"),
+//       require("karma-coverage-istanbul-reporter"),
+//     ],
+//     client: {
+//       clearContext: false,
+//     },
+//     coverageIstanbulReporter: {
+//       dir: require("path").join(__dirname, "./coverage"),
+//       reports: ["html", "lcovonly", "text-summary"],
+//       fixWebpackSourcePaths: true,
+//       thresholds: {
+//         emitWarning: false,
+//         global: {
+//           statements: 100,
+//           lines: 100,
+//           branches: 100,
+//           functions: 100,
+//         },
+//       },
+//     },
+//     reporters: ["progress", "coverage-istanbul"],
+//     port: 9876,
+//     colors: true,
+//     logLevel: config.LOG_INFO,
+//     autoWatch: true,
+//     browsers: ["ChromeHeadless"],
+//     singleRun: false,
+//     restartOnFileChange: true,
+//   });
+// };
+
+// karma.conf.js
 module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage"),
-    ],
-    client: {
-      jasmine: {
-        // Jasmine configuration
-      },
-      clearContext: false,
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true,
-    },
+    // ... other configurations
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage"),
-      subdir: ".",
-      reporters: [
-        { type: "html" },
-        { type: "text-summary" },
-        { type: "lcovonly" },
-      ],
-      // Enforce 100% coverage - CI will fail if below these thresholds
+      // ... other coverage configurations
       check: {
         global: {
           statements: 100,
@@ -61,13 +76,5 @@ module.exports = function (config) {
         },
       },
     },
-    reporters: ["progress", "kjhtml", "coverage"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["Chrome"],
-    singleRun: false,
-    restartOnFileChange: true,
   });
 };
